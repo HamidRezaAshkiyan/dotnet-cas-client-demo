1b422cee77fe2042c579b0f0e2e22d787237c16c
Hamid Reza Ashkiyan <HamidRezaAshkiyan@gmail.com>
2020-07-11 20:38:30 +0430

    added tls12 to global.cs

--- dotnet-cas-client-demo/Global.asax.cs
+++ dotnet-cas-client-demo/Global.asax.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
+using System.Net;
 using System.Web.Routing;
 
 namespace dotnet_cas_client_demo
@@ -13,6 +9,7 @@ namespace dotnet_cas_client_demo
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             RegisterRoutes(RouteTable.Routes);
         }
 
